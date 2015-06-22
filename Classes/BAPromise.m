2bf4b6d5caab0d9847f303a4102de03eb235e2fb
Ben Allison <benski@winamp.com>
2015-06-22 14:34:30 -0400

    m

---
+++ Classes/BAPromise.m
@@ -0,0 +1,13 @@
+//
+//  BAPromise.m
+//  BAPromise
+//
+//  Created by Ben Allison on 6/22/15.
+//  Copyright (c) 2015 Ben Allison. All rights reserved.
+//
+
+#import "BAPromise.h"
+
+@implementation BAPromise
+
+@end
