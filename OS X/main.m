71817a46f592b2b6760ba13f84e40b7669e939c0
Ben Allison <benallison@iheartmedia.com>
2015-08-14 14:41:03 -0400

    OS X support

---
+++ OS X/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OS X
+//
+//  Created by Ben Allison on 8/14/15.
+//  Copyright © 2015 Ben Allison. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
